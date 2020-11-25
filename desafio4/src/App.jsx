9a5eeb99bf07766dc7aaee91f26807a55d8078c8
Herbert Nordson <herbert_nordson@hotmail.com>
2020-11-24 21:02:28 -0300

    Commit: Elaboração de tela de Login

--- desafio4/src/App.jsx
+++ desafio4/src/App.jsx
@@ -4,7 +4,25 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-		
+		<div className="login">
+			<div className="topo">
+
+			</div>
+			<div className="dados">
+				<div className="email">
+					<span>Email</span><br></br>
+					<input type="text" placeholder="exemplo@gmail.com" />
+				</div>
+				<div className="senha">
+					<span>Senha</span><br></br>
+					<input type="text" />
+				</div>
+				<span className="recuperarSenha">Esqueci minha senha</span>
+			</div>
+			<div className="entrar">
+				<button>Entrar</button>
+			</div>
+		</div>
     </div>
   );
 }
